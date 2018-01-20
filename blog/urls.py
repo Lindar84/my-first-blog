9e355c5e41d9a49ed2e8a029693e4656f5be69f3
Ludmila <l.stastova@seznam.cz>
2018-01-20 17:56:32 +0100

    form

--- blog/urls.py
+++ blog/urls.py
@@ -2,4 +2,7 @@ from django.conf.urls import url
 from . import views
 
 # fce vraci seznam prispevku na hlavni strance:
-urlpatterns = [url('^$', views.post_list, name='post_list')]    # ^ je zacatek a $ konec retezce
+urlpatterns = [url('^$', views.post_list, name='post_list'),    # ^ je zacatek a $ konec retezce
+               url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
+               url('^post/new$', views.post_new, name='post_new'),
+               ]
